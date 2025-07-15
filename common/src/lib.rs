3bd548a1a892e82cff008ab2d64c22ff2d13214d
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2025-07-15 15:06:21 +0200

    Refactor file reading functions to use crate root instead of workspace root

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,17 +1,16 @@
 use std::fmt::Debug;
 use std::fs;
+use std::path::Path;
 use std::str::FromStr;
 
 pub fn read_file_as_string(path: &str) -> String {
-    let ws_root = workspace_root::get_workspace_root();
-    let path = ws_root.join(path);
+    let path = get_path_from_root(path);
     let input = fs::read_to_string(path).expect("Something went wrong reading the file");
     input.trim().to_string()
 }
 
 pub fn read_file_as_lines(path: &str) -> Vec<String> {
-    let ws_root = workspace_root::get_workspace_root();
-    let path = ws_root.join(path);
+    let path = get_path_from_root(path);
     fs::read_to_string(path)
         .expect("Something went wrong reading the file")
         .lines()
@@ -21,11 +20,15 @@ pub fn read_file_as_lines(path: &str) -> Vec<String> {
 
 pub fn read_file_as_elements<T>(path: &str) -> Vec<T>
  where T: FromStr, <T as FromStr>::Err: Debug {
-    let ws_root = workspace_root::get_workspace_root();
-    let path = ws_root.join(path);
+    let path = get_path_from_root(path);
     fs::read_to_string(path)
         .expect("Something went wrong reading the file")
         .lines()
         .map(|s| s.parse().unwrap())
         .collect()
+}
+
+fn get_path_from_root(path: &str) -> String {
+    let crate_root = Path::new(env!("CARGO_MANIFEST_DIR"));
+    crate_root.parent().unwrap().join(path).to_str().unwrap().to_string()
 }
