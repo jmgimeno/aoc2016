2bcd6d52bc8a75fc3a8d4635a24fbeffd7496056
Juan Manuel Gimeno <jmgimeno@gmail.com>
2025-07-15 12:25:38 +0200

    Add Day 1 solution for Advent of Code 2016

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Advent of Code 2016
 
+- Day 1: No Time for a Taxicab
---
+++ day01/src/main.rs
@@ -0,0 +1,212 @@
+use std::str::FromStr;
+
+fn main() {
+    let input = load_input("data/day01.txt");
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+}
+
+fn part1(input: &[Turn]) -> u32 {
+    let mut state = State::default();
+    for turn in input {
+        state.step(turn);
+    }
+    state.distance()
+}
+
+fn part2(input: &[Turn]) -> u32 {
+    let mut visited = std::collections::HashSet::new();
+    visited.insert((0, 0));
+    let mut state = State::default();
+    for turn in input {
+        let steps = state.step_with_trace(turn);
+        for (x, y) in steps {
+            if visited.contains(&(x, y)) {
+                return (x.abs() + y.abs()) as u32;
+            }
+            visited.insert((x, y));
+        }
+    }
+    panic!("Should never get here");
+}
+
+fn load_input(path: &str) -> Vec<Turn> {
+    common::read_file_as_string(path)
+        .split(", ")
+        .map(|s| s.parse().unwrap())
+        .collect()
+}
+
+#[derive(Debug, Default)]
+struct State {
+    x: i32,
+    y: i32,
+    heading: Direction,
+}
+
+impl State {
+    fn step(&mut self, turn: &Turn) {
+        self.heading = self.heading.turn(turn);
+        self.forward(turn.steps())
+    }
+    fn forward(&mut self, steps: u32) {
+        match self.heading {
+            Direction::North => self.y += steps as i32,
+            Direction::East => self.x += steps as i32,
+            Direction::South => self.y -= steps as i32,
+            Direction::West => self.x -= steps as i32,
+        }
+    }
+    fn step_with_trace(&mut self, turn: &Turn) -> Vec<(i32, i32)> {
+        self.heading = self.heading.turn(turn);
+        self.forward_with_trace(turn.steps())
+    }
+    fn forward_with_trace(&mut self, steps: u32) -> Vec<(i32, i32)> {
+        match self.heading {
+            Direction::North => {
+                let trace = (self.y + 1..=self.y + steps as i32)
+                    .map(|y| (self.x, y))
+                    .collect();
+                self.y += steps as i32;
+                trace
+            }
+            Direction::East => {
+                let trace = (self.x + 1..=self.x + steps as i32)
+                    .map(|x| (x, self.y))
+                    .collect();
+                self.x += steps as i32;
+                trace
+            }
+            Direction::South => {
+                let trace = (self.y - steps as i32..=self.y - 1)
+                    .map(|y| (self.x, y))
+                    .rev()
+                    .collect();
+                self.y -= steps as i32;
+                trace
+            }
+            Direction::West => {
+                let trace = (self.x - steps as i32..=self.x - 1)
+                    .map(|x| (x, self.y))
+                    .rev()
+                    .collect();
+                self.x -= steps as i32;
+                trace
+            }
+        }
+    }
+    fn distance(&self) -> u32 {
+        (self.x.abs() + self.y.abs()) as u32
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+impl Default for Direction {
+    fn default() -> Self {
+        Direction::North
+    }
+}
+
+impl Direction {
+    fn turn(&self, turn: &Turn) -> Self {
+        match turn {
+            Turn::Left(_) => self.left(),
+            Turn::Right(_) => self.right(),
+        }
+    }
+    fn left(&self) -> Self {
+        match self {
+            Direction::North => Direction::West,
+            Direction::East => Direction::North,
+            Direction::South => Direction::East,
+            Direction::West => Direction::South,
+        }
+    }
+    fn right(&self) -> Self {
+        match self {
+            Direction::North => Direction::East,
+            Direction::East => Direction::South,
+            Direction::South => Direction::West,
+            Direction::West => Direction::North,
+        }
+    }
+}
+#[derive(Debug)]
+enum Turn {
+    Left(u32),
+    Right(u32),
+}
+
+impl FromStr for Turn {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.trim();
+        let steps = &s[1..].parse::<u32>().unwrap();
+        if s.starts_with("L") {
+            Ok(Turn::Left(*steps))
+        } else if s.starts_with("R") {
+            Ok(Turn::Right(*steps))
+        } else {
+            Err(format!("Invalid turn: {}", s))
+        }
+    }
+}
+
+impl Turn {
+    fn steps(&self) -> u32 {
+        match self {
+            Turn::Left(steps) => *steps,
+            Turn::Right(steps) => *steps,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use Turn::*;
+
+    #[test]
+    fn test_example1_part1() {
+        let input = vec![Right(2), Left(3)];
+        assert_eq!(part1(&input), 5);
+    }
+
+    #[test]
+    fn test_example2_part1() {
+        let input = vec![Right(2), Right(2), Right(2)];
+        assert_eq!(part1(&input), 2);
+    }
+
+    #[test]
+    fn test_example3_part1() {
+        let input = vec![Right(5), Left(5), Right(5), Right(3)];
+        assert_eq!(part1(&input), 12);
+    }
+
+    #[test]
+    fn test_example1_part2() {
+        let input = vec![Right(8), Right(4), Right(4), Right(8)];
+        assert_eq!(part2(&input), 4);
+    }
+
+    #[test]
+    fn test_part1() {
+        let input = load_input("../data/day01.txt");
+        assert_eq!(part1(&input), 300);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = load_input("../data/day01.txt");
+        assert_eq!(part2(&input), 159);
+    }
+}
