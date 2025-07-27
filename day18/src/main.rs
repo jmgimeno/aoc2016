a9fc90f760c38e67ee89b77ad68b72d80d552b0e
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2025-07-27 10:33:29 +0200

    Refactor Day 18 part function: streamline row processing and eliminate unnecessary embedding

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,8 +1,7 @@
 use once_cell::sync::Lazy;
 
 static FIRST_ROW: Lazy<String> =
-    Lazy::new(|| common::read_file_as_string("data/day18.txt").unwrap()
-);
+    Lazy::new(|| common::read_file_as_string("data/day18.txt").unwrap());
 
 fn main() {
     println!("Part 1: {}", part(&FIRST_ROW, 40));
@@ -10,39 +9,30 @@ fn main() {
 }
 
 fn part(input: &str, num_steps: usize) -> usize {
-    let mut embed = embed(input);
-    let mut total_safe = count_safe(&embed);
+    let width = input.len();
+    let mut row = input.as_bytes().to_vec();
+    let mut total_safe = row.iter().filter(|&&c| c == b'.').count();
+
+    let mut next_row = vec![b'.'; width];
     for _ in 1..num_steps {
-        let mut new_embed = embed.clone();
-        for i in 1..embed.len() - 1 {
-            let left = embed[i - 1];
-            let center = embed[i];
-            let right = embed[i + 1];
-            if (left == b'^' && center == b'^' && right != b'^') ||
-                (left != b'^' && center == b'^' && right == b'^') ||
-                (left == b'^' && center != b'^' && right != b'^') ||
-                (left != b'^' && center != b'^' && right == b'^') {
-                new_embed[i] = b'^';
+        for i in 0..width {
+            let left = if i == 0 { b'.' } else { row[i - 1] };
+            let center = row[i];
+            let right = if i == width - 1 { b'.' } else { row[i + 1] };
+            next_row[i] = if (left == b'^' && center == b'^' && right != b'^')
+                || (left != b'^' && center == b'^' && right == b'^')
+                || (left == b'^' && center != b'^' && right != b'^')
+                || (left != b'^' && center != b'^' && right == b'^')
+            {
+                b'^'
             } else {
-                new_embed[i] = b'.';
-            }
+                b'.'
+            };
         }
-        total_safe += count_safe(&new_embed);
-        embed = new_embed;
+        total_safe += next_row.iter().filter(|&&c| c == b'.').count();
+        std::mem::swap(&mut row, &mut next_row);
     }
     total_safe
-}
-
-fn embed(input: &str) -> Vec<u8> {
-    let mut row = Vec::with_capacity(input.len() + 2);
-    row.push(b'.');
-    row.extend(input.as_bytes());
-    row.push(b'.');
-    row
-}
-
-fn count_safe(embed: &[u8]) -> usize {
-    embed.iter().filter(|&&c| c == b'.').count() - 2
 }
 
 #[cfg(test)]
